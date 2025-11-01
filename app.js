df45682cbb4b2a808e948d446a1d95bd9c6c15fa
ewras <113038300+ewrast@users.noreply.github.com>
2025-11-01 20:33:52 +0300

    Resolve timezone issues in date handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-01T17:33:24.923Z
-Commit: Add multi-language support
+Timestamp: 2025-11-01T17:33:52.924Z
+Commit: Resolve timezone issues in date handling
