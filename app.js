5100cdad2922d7cffd47fad0bcdc73b370f33bf7
ewras <113038300+ewrast@users.noreply.github.com>
2025-11-01 20:33:19 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-01T17:32:24.169Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-01T17:33:20.156Z
+Commit: Fix CSS styling bug on mobile devices
