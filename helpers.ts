8252faf4c7e2cd98141e4fb15f55b88e51170271
ewras <113038300+ewrast@users.noreply.github.com>
2025-11-01 20:33:11 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-01T17:32:43.104Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-01T17:33:11.949Z
+Commit: Resolve timezone issues in date handling
