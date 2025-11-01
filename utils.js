8b944c362749176db55f7f0798f94db3b8e8f3f9
ewras <113038300+ewrast@users.noreply.github.com>
2025-11-01 20:32:31 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-01T17:32:16.316Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-01T17:32:31.557Z
+Commit: Resolve null pointer exception in user service
