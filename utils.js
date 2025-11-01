48d38c19196aa0216d9b503ca695fb27641e072a
ewras <113038300+ewrast@users.noreply.github.com>
2025-11-01 20:31:11 +0300

    Correct typo in configuration file

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-01T17:31:07.634Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-01T17:31:11.475Z
+Commit: Correct typo in configuration file
