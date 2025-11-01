c5712bbcc0c0ebeb3aade6b36000e0e55d2e20c3
ewras <113038300+ewrast@users.noreply.github.com>
2025-11-01 20:32:12 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-01T17:32:03.488Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-01T17:32:12.767Z
+Commit: Resolve null pointer exception in user service
