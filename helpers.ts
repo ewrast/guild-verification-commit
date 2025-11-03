27795b2a0b64cfed3d5b6909f9aa8192d2536125
ewras <113038300+ewrast@users.noreply.github.com>
2025-11-03 09:34:25 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-01T17:34:10.758Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-03T06:34:25.204Z
+Commit: Resolve null pointer exception in user service
