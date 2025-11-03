e03a36654ed64f9e67f41bf17cdf581380626b76
ewras <113038300+ewrast@users.noreply.github.com>
2025-11-03 09:34:58 +0300

    Implement user profile customization

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-03T06:34:25.204Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-03T06:34:58.255Z
+Commit: Implement user profile customization
