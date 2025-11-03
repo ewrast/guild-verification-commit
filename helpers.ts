cb007196928661b561a061b7b3f692acb8ac8d03
ewras <113038300+ewrast@users.noreply.github.com>
2025-11-03 09:34:59 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-03T06:34:58.255Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-03T06:34:59.551Z
+Commit: Fix race condition in async operations
