48efcd3aba8ee12fa1ae526c1139af6cd59fd030
ewras <113038300+ewrast@users.noreply.github.com>
2025-11-05 07:44:31 +0300

    Add multi-language support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T04:44:08.466Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-05T04:44:29.413Z
+Commit: Add multi-language support
