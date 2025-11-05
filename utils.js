99be56b63719e906e5b3837a89a08d0099273ae0
ewras <113038300+ewrast@users.noreply.github.com>
2025-11-05 07:47:00 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T04:44:53.367Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-05T04:46:58.306Z
+Commit: Fix CSS styling bug on mobile devices
