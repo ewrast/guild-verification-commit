79a5b8f729fb7a6c0f5cb3b64cc446f94577081b
ewras <113038300+ewrast@users.noreply.github.com>
2025-11-05 07:41:49 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T04:40:45.129Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-05T04:41:48.074Z
+Commit: Fix CSS styling bug on mobile devices
