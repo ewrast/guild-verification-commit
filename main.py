744275546f8e87c80f9d3902f05639627d1bdb45
ewras <113038300+ewrast@users.noreply.github.com>
2025-11-05 07:48:50 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T04:48:29.002Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-05T04:48:49.108Z
+Commit: Introduce real-time notifications
