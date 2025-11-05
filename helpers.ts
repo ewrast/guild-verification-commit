8406655746e67a304c84effbfc83092440d3885e
ewras <113038300+ewrast@users.noreply.github.com>
2025-11-05 07:42:54 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T04:41:50.228Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-05T04:42:52.422Z
+Commit: Patch API endpoint error handling
