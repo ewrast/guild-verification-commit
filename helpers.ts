58f18df27a086e593e1187518157981355e217fa
ewras <113038300+ewrast@users.noreply.github.com>
2025-11-05 07:43:37 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T04:42:52.422Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-05T04:43:35.350Z
+Commit: Fix memory leak in event handlers
