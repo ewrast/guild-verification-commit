f9960509870c66f336d99b1425297ef0a1c11e30
ewras <113038300+ewrast@users.noreply.github.com>
2025-11-05 07:40:01 +0300

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T04:39:45.816Z
-Commit: Add multi-language support
+Timestamp: 2025-11-05T04:39:59.341Z
+Commit: Resolve timezone issues in date handling
