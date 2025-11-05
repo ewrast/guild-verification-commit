158a73e8d25f43d5e60c8df7ff4b69b8757b19a0
ewras <113038300+ewrast@users.noreply.github.com>
2025-11-05 07:47:34 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T04:46:37.712Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-05T04:47:31.991Z
+Commit: Fix memory leak in event handlers
