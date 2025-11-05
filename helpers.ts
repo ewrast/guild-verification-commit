045c463008c40893db2f8d5a885958f087d28874
ewras <113038300+ewrast@users.noreply.github.com>
2025-11-05 07:46:55 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T04:44:50.870Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-05T04:46:53.761Z
+Commit: Patch security vulnerability in authentication
